5ed8f459f899f9cdf4f1f35feb50becf5441d460
cooper <cooperfstein@icloud.com>
2026-07-18 09:59:14 -0500

    Initial Commit

---
+++ FirstGigTests/FirstGigTests.swift
@@ -0,0 +1,18 @@
+//
+//  FirstGigTests.swift
+//  FirstGigTests
+//
+//  Created by Cooper Stein on 7/18/26.
+//
+
+import Testing
+
+struct FirstGigTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
